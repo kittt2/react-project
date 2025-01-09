cbe57c0e090df86f75c10dfb82e9e93a18cb0c50
Nitin <145486768+kittt2@users.noreply.github.com>
2025-01-09 13:52:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-origin: ["https://eccomerce-platfom.onrender.com", "https://nitinproject01.netlify.app","http://localhost:5173/","https://mern-project-frontend-one.vercel.app/"],    
+origin: ["http://localhost:5173/","https://mern-project-frontend-one.vercel.app/"],    
     optionsSuccessStatus: 200, 
   })
 );
